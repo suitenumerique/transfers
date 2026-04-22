7d3bbd1c474fbb7f023313de53758668c4fb6320
Stanislas Bruhiere <stanislas@bruhiere.fr>
2026-04-22 18:42:06 +0200

    (frontend) bugfixes + wording

--- src/frontend/src/pages/_document.tsx
+++ src/frontend/src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <meta name="theme-color" content="#FFFFFF" />
+        <link rel="icon" type="image/svg+xml" href="/images/transferts-favicon.svg" />
+        <link rel="alternate icon" href="/images/transferts-favicon.svg" />
+        <link rel="apple-touch-icon" href="/images/transferts-favicon.svg" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="application-name" content="Transferts" />
         <meta name="description" content="Service de transfert de fichiers" />
